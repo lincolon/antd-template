84254581c08a30b6a694137b0c73deefc16d908f
lincolon <514278952@qq.com>
2022-01-20 17:23:26 +0800

    列表查询状态记录

--- src/components/ProTable/index.jsx
+++ src/components/ProTable/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ProTable from '@ant-design/pro-table';
 
 import { useSearchParams } from 'react-router-dom';
@@ -7,25 +7,38 @@ import { urlToObject } from '../../utils/helper';
 
 export default function Table(props){
 
+    const formRef = useRef();
+
     let [searchParams, setSearchParams] = useSearchParams();
     const searchFormValues = urlToObject(searchParams);
 
     if(searchFormValues.start_at){
-        searchFormValues.created_at = [searchFormValues.start_at, searchFormValues.end_at];
-    } 
+      searchFormValues.created_at = [searchFormValues.start_at, searchFormValues.end_at];
+    }
+
+    useEffect(() => {
+      formRef.current && formRef.current.setFieldsValue(searchFormValues);
+    }, []);
 
     return <ProTable 
             rowKey="id"
-            form={{initialValues: searchFormValues}}
-            onSubmit={(params) => setSearchParams(params)}
-            search={{
-              labelWidth: 'auto',
-              span: 6
+            formRef={formRef}
+            onSubmit={(params) => {
+              setSearchParams({...params, page: searchFormValues.page || 1})
+            }}
+            onReset={() => {setSearchParams({page:1});formRef.current.resetFields()}}
+            search={{labelWidth: 'auto', span: 6}}
+            onChange={(pagination) => {
+              setSearchParams({...searchFormValues, page: pagination.current})
             }}
             pagination={{
               pageSize: 20,
+              defaultCurrent: searchFormValues.page || 1,
             }}
             defaultSize="small"
             {...props}
+            request={async (params = {}) => {
+              return await props.request({...params, current: searchFormValues.page || 1})
+            }}
         />
 }
